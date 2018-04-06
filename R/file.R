9ea763bb498b66beaa55d8c0e94725acaee94d6f
soohyuna <ahnsooh1@msu.edu>
2018-04-06 17:18:54 -0400

    first commit

---
+++ R/file.R
@@ -0,0 +1,15 @@
+#' A package to analyze qPCR data from CT values
+#'
+#' @param object A package
+#' @return An analysis output of relative FC
+#' @example
+#'
+#' @export
+
+
+
+
+
+function(file, range) {
+
+}
