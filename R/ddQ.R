fe45ddac976f8798ac1f852e1d963b6ad40d4976
nguyens7 <nguyens7@msu.edu>
2018-05-11 13:30:39 -0400

    Added ddQ

---
+++ R/ddQ.R
@@ -0,0 +1,39 @@
+#' A function to calculate delta delta CT values by groups
+#'
+#' @param df A data frame.
+#' @param  ... Variables to group by.
+#' @param col Column containing group of interest.
+#' @param control String value for control group of interest.
+#' @param na.rm Remove NAs from data, defaults to TRUE.
+#' @return Data frame of average CT values by groups and number of values averaged.
+#' @example
+#'   df %>% avgQ(sheet, Mouse, State, Gene,
+#'               name = "Tech_rep",
+#"               param_var = CT)
+#'
+#' @keywords summarize, mean, average
+#' @import tidyverse
+#'
+#' @export
+
+
+
+ddQ <- function(df, ..., col, control, na.rm = TRUE) {
+
+  group_var <- quos(...)
+  col <- enquo(col)
+  cont_deltaCT <- paste(control, "deltaCT", sep= "_")
+  cont_deltaCT2 <- as.name(cont_deltaCT)
+
+
+  cont_deltaCT_df <- df %>%
+    filter(!! col == control ) %>%
+    group_by(!!! group_var) %>%
+    summarise(!! cont_deltaCT := mean(deltaCT, na.rm = na.rm))
+
+  df %>%
+    inner_join(cont_deltaCT_df) %>%
+    mutate(ddCT = deltaCT - UQ(cont_deltaCT2),
+           FC = 2^-(ddCT))
+
+}
