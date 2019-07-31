e47dedaa1ecb6a02f68b871abc30eacdd356ca16
nguyens7 <nguyens7@msu.edu>
2019-07-31 14:57:42 -0400

    Added sep_var argument to cleanQ

--- R/cleanQ.R
+++ R/cleanQ.R
@@ -1,26 +1,36 @@
 #' A function to clean a xls imported data frame
 #'
 #' @param df A data frame.
+#' @param sep_samp Separate sample column into individual columns.
 #' @param  sep_var Variables of variables to be separated.
 #' @return Data frame suitable for downstream tidyQ analysis.
 #' @keywords clean, tidy, import
 #' @export
 
-cleanQ <- function(df, sep_var) {
+cleanQ <- function(df, sep_samp = FALSE,  sep_var) {
 
 rawCT <- paste0("C","\u0442")
 
-  df %>%
-    rename("Sample" = `Sample Name`,
+  clean_df <- df %>%
+    dplyr::rename("Sample" = `Sample Name`,
            "Gene" = `Target Name`,
            "CT" = rawCT) %>%
     dplyr::select(-(Task:`RQ Max`)) %>%
-    tidyr::separate(Sample,
-                    into = sep_var,
-                    sep = "_") %>%
     dplyr::filter(!CT %in% c("NA","Undetermined")) %>%
     dplyr::mutate_at(vars(1:Gene), as.factor) %>%
     dplyr::mutate_at(vars(CT), as.numeric)
+
+  return(clean_df)
+
+
+  if(sep_samp == TRUE){
+
+  clean_df %>%
+      tidyr::separate(Sample,
+                      into = sep_var,
+                      sep = "_")
+
+  }
 
 }
 
